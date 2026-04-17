5bb4a485269b8c5715d47bd85d240e75eb6c122e
QERR0R <n0n4m3007err0r@gmail.com>
2026-04-17 22:18:39 +0800

    Removed useless configs & modified as well

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,8 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Removes deafult fish greeting banner.
 set fish_greeting
 
+
+# running pywal with color.py script that will prepare the .Xresources that will be used by dwm for changing a color in realtime.
 function set_wallpaper
     set path $argv[1]
     wal -n -i $path
